2caf527132b365a38d65249bfe91294ec0137c79
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-11-11 09:38:14 +0000

    Minor alterations to tidy_functions, added test for read_ceda_file()

---
+++ R/tidy_functions.R
@@ -0,0 +1,49 @@
+# This script reads the raw .per (text) tiles downloaded via FTP from CEDA,
+# tidies them, and combines them into a single data frame
+
+# Loads required packages
+library(tidyverse)
+library(PFUSetup)
+library(testthat)
+
+
+# Establishes a function which reads an individual CEDA .per (text) file
+read_ceda_file <- function (ceda_file) {
+  readr::read_table2(ceda_file,
+                     col_types = cols(YEAR = col_double(),
+                                      JAN = col_double(), FEB = col_double(),
+                                      MAR = col_double(), APR = col_double(),
+                                      MAY = col_double(), JUN = col_double(),
+                                      JUL = col_double(), AUG = col_double(),
+                                      SEP = col_double(), OCT = col_double(),
+                                      NOV = col_double(), DEC = col_double(),
+                                      MAM = col_double(), JJA = col_double(),
+                                      SON = col_double(), DJF = col_double(),
+                                      ANN = col_double()
+                                      ),
+                     skip = 3) %>%
+    tibble::tibble() %>%
+    dplyr::mutate(Country = substr(basename(ceda_file), 23, nchar(basename(ceda_file))-8), .before = YEAR)
+}
+
+
+# Establishes a function which uses read_ceda_file() to read all the CEDA files
+# for a particular metric and binds them into a single tibble
+read_ceda_files <- function (ceda_metric, ceda_year, data) {
+
+  dest_file = paste0(PFUSetup::get_abs_paths()$project_path,
+                         "/Data/CEDA Data/CEDA_", ceda_year, "/", ceda_metric, sep ="")
+
+  dirs = paste0(dest_file, "/", list.files(path = dest_file), sep = "")
+
+  data <- do.call(rbind, purrr::map(dirs, read_ceda_file))
+
+}
+
+# tmp_2019 <- read_ceda_files(ceda_metric = "tmp", ceda_year = 2019)
+#
+# tmx_2019 <- read_ceda_files(ceda_metric = "tmx", ceda_year = 2019)
+#
+# tmp_2020 <- read_ceda_files(ceda_metric = "tmp", ceda_year = 2020)
+#
+# tmx_2020 <- read_ceda_files(ceda_metric = "tmx", ceda_year = 2020)
---
+++ tests/testthat/test-tidy_functions.R
@@ -0,0 +1,44 @@
+# Loads required packages
+library(testthat)
+
+# This tests the function read_ceda_file()
+test_that("read_ceda_file() works", {
+
+  # Defines a file path for CEDA 2020 tmp Actaeon_Group data to test
+  test_filepath = paste0(PFUSetup::get_abs_paths()$project_path,
+                         "/Data/CEDA Data/CEDA_2020/tmp/crucy.v4.04.1901.2019.Actaeon_Group.tmp.per",
+                         sep ="")
+
+
+  test_data <- read_ceda_file(ceda_file = test_filepath)
+
+
+  expect_true(!is.null(test_data))
+  expect_true(identical(colnames(test_data),
+                        c("Country", "YEAR", "JAN", "FEB", "MAR", "APR", "MAY",
+                          "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "MAM",
+                          "JJA", "SON", "DJF", "ANN")) == TRUE
+              )
+
+
+
+})
+
+
+# test_that("read_ceda_files() works as expected", {
+#
+#   test_2 <- tibble::tribble(~COUNTRY, ~YEAR, ~JAN, ~FEB, ~MAR, ~APR, ~MAY, ~JUN. ~JUL,
+#                             ~AUG, ~SEP, ~OCT, ~NOV, ~DEC, ~MAM, ~JJA, ~SON,
+#                             ~DJF, ~ANN,
+#                             "Actaeon_Group", 1901, 26.1, 26.5, 26.5, 25.9, 24.6, 23.5, 22.9,
+#                             22.6, 22.8, 23.5, 24.4, 25.2, 25.6, 23.0, 23.6,
+#                             25.9, 24.5)
+#   read_ceda_files()
+#
+#   expect_true(!is.null(test_2))
+#   expect_true(length(unique(test_2$)))
+#
+#
+# })
+
+#####################################################################################################################
