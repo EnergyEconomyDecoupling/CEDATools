460aa01e2666c6f2171abbf9cc35f31e1aa9a72b
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-11-18 17:08:45 +0000

    Thinking about how to to test down_functions.R

--- tests/testthat/test-down_functions.R
+++ tests/testthat/test-down_functions.R
@@ -1,15 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("down_cru_cy_data() works", {
+
+  # down_cru_cy_data(username = "zmarshall",
+  #                  password = "ParrotIron4",
+  #                  cru_cy_metric = "wet",
+  #                  cru_cy_folder = file.path(PFUSetup::get_abs_paths()$project_path,
+  #                                            "Data", "CEDA Data", "CEDA_2020")
+  # )
+
+  expect_true(!is.null(cru_cy_folder))
+  expext
+
 })
 
-
-down_cru_cy_data(username = "zmarshall",
-                 password = "ParrotIron4",
-                 cru_cy_metric = "wet",
-                 cru_cy_folder = file.path(PFUSetup::get_abs_paths()$project_path,
-                                           "Data", "CEDA Data", "CEDA_2020")
-)
-
-username = "zmarshall"
-password = "ParrotIron4"
-cru_cy_metric = "vap"
